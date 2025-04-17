72c4310ff0d3cc00bf30ab3720ee3e28bab0d07d
Prashant <prashantdos2020@gmail.com>
2025-04-17 23:37:40 +0530

    checking 2

--- server/index.js
+++ server/index.js
@@ -20,7 +20,6 @@ const app = express();
 if (process.env.NODE_ENV === "production") {
   console.log = () => {};
 }
-
 // var storage,upload;
 const connection = mongoose
   .connect(process.env.MONGO_URL)
